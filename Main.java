048f8178b577860366b1bebeaab3f5764885b9ff
Shivani Manikandan <77360403+shookvani@users.noreply.github.com>
2024-09-28 15:46:44 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,11 @@ public class Main {
         Battleship.placeShips();
         boolean playing = true;
 
-        String[] leaders = {"Iven - 310", "Schmidty bang bang - 295", "Good Luck Charlie -" +
-                " 285", "Matthew - 280", "Charles Edwards - 280", "Sean - 270" , "Vicky Calvin - " +
-                "265", "Nathan - 230"};
-
         GameSetup.println("Welcome to Battleship!\n");
-        GameSetup.leaderboard(leaders);
+        GameSetup.leaderboard(Battleship.getLeaders());
         GameSetup.how();
         GameSetup.play(playing, scan);
         GameSetup.results();
     }
+
 }
